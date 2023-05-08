5ec590ffbc3f5821b44feb7132ac9fe7725f48f1
Bigizic <olalekanisaac75@gmail.com>
2023-05-08 18:38:48 +0000

    update_check_cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+/* header */
+
+/**
+ * check_cycle - checks if a single linked list has a cycle in it
+ *
+ * @: list: list to check
+ *
+ * Return: 0 if there's no list else return 1
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *input = list;
+	listint_t *word = list;
+
+	if (list == NULL)
+		return (NULL);
+
+	while (input && input->next)
+	{
+		word = word->next;
+		input = input->next->next;
+
+		if (word == input)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
