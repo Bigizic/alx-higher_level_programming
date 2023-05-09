daf72c95f22f232e8a5fbdf3f2930d7a29090c63
Bigizic <olalekanisaac75@gmail.com>
2023-05-09 15:48:11 +0000

    update_print_comb2

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,2 @@
-#!/usr/bin/python3
-for i in range(100):
-    print("{:02d}".format(i), end = ", " if i < 99 else "\n")
+ in range(100):
+     print("{:02d}".format(i), end=", " if i < 99 else "\n")
