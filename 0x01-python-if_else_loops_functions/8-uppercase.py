07825ddedfcad01d87042af2a289acc410a9d6ca
Bigizic <olalekanisaac75@gmail.com>
2023-05-09 18:00:00 +0000

    Update -uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    word = ""
+    for x in str:
+        if ord(x) > 96 and ord(x) < 123:
+            word += chr(ord(x)-32)
+        else:
+            word += x
+            print("{}".format(word))
