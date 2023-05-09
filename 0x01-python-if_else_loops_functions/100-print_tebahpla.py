d5a809098846e774b8bf1b0e95b21399d29f5ece
Bigizic <olalekanisaac75@gmail.com>
2023-05-09 23:37:36 +0000

    Update 00-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for i in range(122, 64, - 1):
-    print("{}".format(chr(i - 32) if i % 2 == 0 else chr(i)), end="")
+for i in range(122, 96, - 1):
+    if i % 2 == 0:
+        print("{}".format(chr(i)), end="")
+    else:
+        print(chr(i - 32), end="")
