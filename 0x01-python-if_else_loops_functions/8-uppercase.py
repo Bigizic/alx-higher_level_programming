4c3e4a7a030408be3194fa8b6687230843617c86
Bigizic <olalekanisaac75@gmail.com>
2023-05-10 17:01:45 +0000

    Update -uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-
-def uppercase(str):
-    return my_upp(str)
-
-def my_upp(st):
-    word = ""
-    for i in st:
-        if 97 <= ord(i) <= 122:
-            word += chr(ord(i) - 32)
+def my_upper(string):
+    res = ""
+    for char in string:
+        if 'a' <= char <= 'z':
+            res += chr(ord(char) - 32)
         else:
-            word += i
-            print("{}".format(word), end="")
-            return word
+            res += char
+    return res
+        
+def uppercase(str):
+    word = str.lower()
+    new_word = my_upper(word)
+    print(new_word)
