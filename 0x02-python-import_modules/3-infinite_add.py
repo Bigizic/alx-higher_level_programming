61708fa56adbfed35911fe0ebd4952fd560bb2c6
Bigizic <117412923+Bigizic@users.noreply.github.com>
2023-05-12 02:05:54 +0100

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,9 +4,11 @@ import sys
 if __name__ == "__main__":
     ac = len(sys.argv)
     word = sys.argv[1:]
-    if ac > 1:
-        result = sum(int(i) for i in word)
-        print("{}".format(result))
+    i = int(word)
+    for i in word:
+        if ac > 1:
+            result = sum(i)
+            print("{}".format(result))
         
     else:
         ac -= 1
