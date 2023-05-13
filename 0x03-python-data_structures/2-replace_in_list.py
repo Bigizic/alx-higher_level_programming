94934e4f85c0ce7010b376a2b5d1bf2158c2150a
Bigizic <olalekanisaac75@gmail.com>
2023-05-13 22:23:54 +0000

    Update -replace_in_list.py

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    l = len(my_list)
+    l -= 1
+    if idx < 0:
+        return my_list
+    if idx > l:
+        return my_list
+    my_list[idx] = element
+    return my_list
