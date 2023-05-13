085a368fe36e85cabcc722144b8f201cba9b5898
Bigizic <olalekanisaac75@gmail.com>
2023-05-13 23:52:38 +0000

    Update -print_matrix_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for x in matrix:
+        for i, n in enumerate(x):
+            if i == len(x) - 1:
+                print("{:d}".format(n), end=" ")
+            else:
+                print("{:d}".format(n), end=" ")
+        print()
