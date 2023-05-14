43dee7ec47b85bf37eab089a91cf4fdf1c5a5af5
Bigizic <olalekanisaac75@gmail.com>
2023-05-14 19:49:16 +0000

    Update -multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     l = len(sentence)
-    f = sentence[0]
+    for i in sentence:
+        f = sentence[0]
     if sentence is None:
         f = None
+        l = None
     return l, f
