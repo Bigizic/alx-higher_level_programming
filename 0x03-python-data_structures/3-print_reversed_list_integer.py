61c034574f4f8ea256f426810a8f1559510a1a5e
Bigizic <olalekanisaac75@gmail.com>
2023-05-15 14:40:27 +0000

    Update print_reversed_list_integer.py

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,5 +2,5 @@
 def print_reversed_list_integer(my_list=[]):
     if my_list:
         word = my_list[::-1]
-            for i in word:
-                print("{:d}".format(i))
+        for i in word:
+            print("{:d}".format(i))
