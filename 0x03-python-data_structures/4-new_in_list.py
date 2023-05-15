dcf3437df6c8f58a5a3d9b14845613f964164ebb
Bigizic <olalekanisaac75@gmail.com>
2023-05-15 14:02:52 +0000

    Update new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -6,8 +6,7 @@ def string_duplicate(string, n):
 def new_in_list(my_list, idx, element):
     word = string_duplicate(my_list, 1)
     list_length = len(my_list)
-    list_length -= 1
-    if idx < 0 or idx > list_length:
+    if idx < 0 or idx >= list_length:
         return my_list
     word[idx] = element
     return word
