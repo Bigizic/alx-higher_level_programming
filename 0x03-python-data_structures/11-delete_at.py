1c196bd67d909b5a3552c8074125f96a7ac04920
Bigizic <olalekanisaac75@gmail.com>
2023-05-15 10:42:46 +0000

    Update 1-delete_at.py

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,5 +3,4 @@ def delete_at(my_list=[], idx=0):
     if idx < 0:
         return my_list
     else:
-        del my_list[idx]
-        return my_list[:idx] + my_list[idx:]
+        return my_list[:idx] + my_list[idx +1:]
