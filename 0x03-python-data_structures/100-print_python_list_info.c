efb566df91b4a5d1b6c676adc3f7a96254a09cd3
Bigizic <olalekanisaac75@gmail.com>
2023-05-16 02:32:45 +0000

    Update print_python_list_info.c

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,33 @@
+#include <Python.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/* header */
+
+/**
+* print_python_list_info - prints some basic info about python lists
+*
+* @p: PyObject
+*
+* Return: basic info about a Python lists
+*/
+
+void print_python_list_info(PyObject *p)
+{
+	Py_ssize_t p_size = 0;
+	int i;
+
+	if(PyList_CheckExact(p))
+	{
+		p_size = PyList_Size(p);
+		printf("[*] Size of the Python List = %zd\n", p_size);
+		printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
+
+		while (i < p_size)
+		{
+			printf("Element %d: %s\n", i,
+					Py_TYPE(PyList_GetItem(p, i))->tp_name);
+			i++;
+		}
+	}
+}
