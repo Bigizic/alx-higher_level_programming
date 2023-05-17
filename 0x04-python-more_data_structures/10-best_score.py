b1f81e0fc5e5996477843ac1904c1017d9a823ca
Bigizic <olalekanisaac75@gmail.com>
2023-05-17 19:28:04 +0000

    Update best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,18 +3,18 @@
 def best_score(a_dictionary):
     if a_dictionary:
         count = 0
-        v = list(a_dictionary.values())
-        k = list(a_dictionary.keys())
-        first = v[0]
-        for i in v:
+        valu = list(a_dictionary.values())
+        key = list(a_dictionary.keys())
+        first = valu[0]
+        for i in valu:
             if i > first:
                 first = i
-        for x in v:
+        for x in valu:
             if first != x:
                 count += 1
             else:
                 break
             
-        return k[count]
+        return key[count]
     else:
         return None
