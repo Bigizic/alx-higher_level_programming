4bc113bd2850c6a05755f8f8eaac633136fb40cd
Bigizic <olalekanisaac75@gmail.com>
2023-05-17 20:48:13 +0000

    Update print_sorted_dictionary.py

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 
 def print_sorted_dictionary(a_dictionary):
+    sorted_keys = sorted(a_dictionary.keys())
+    for i in sorted_keys:
+        values = a_dictionary[i]
+        print(f"{i}: {values}")
