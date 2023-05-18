a374a7b62bbc0a1abe4217b893440e8d0163cca0
Bigizic <olalekanisaac75@gmail.com>
2023-05-18 02:43:34 +0000

    Update weight_average.py

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def weight_average(my_list=[]):
+    if my_list:
+        result = 0
+        new_result = 0
+        for i in my_list:
+            result += i[0] *i[1]
+            new_result += i[1]
+        if new_result != 0:
+            return result / new_result
+    else:
+        return 0
