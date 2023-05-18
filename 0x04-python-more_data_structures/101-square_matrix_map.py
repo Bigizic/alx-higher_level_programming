8d49962b1198eb636bdb5115767d7f980c18dc1e
Bigizic <olalekanisaac75@gmail.com>
2023-05-18 03:50:31 +0000

    Update square_matrix_map.py

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(map(lambda x: [i ** 2 for i in x], matrix))
+    return list(map(lambda x: list(map(lambda y: y ** 2, x)),  matrix))
