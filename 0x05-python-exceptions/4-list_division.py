69d2ba095f41b94b85fe6f01d1223ca860ec8c55
Bigizic <olalekanisaac75@gmail.com>
2023-05-29 18:18:41 +0000

    Update list_division.py

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -24,7 +24,8 @@ def list_division(my_list_1, my_list_2, list_length):
             else:
                 result = my_list_1[x] / my_list_2[x]
 
-            new_list.append(result)
+            formatted_res = '{:.1f}'.format(result).rstrip('0').rstrip('.')
+            new_list.append(float(formatted_res) if '.' in formatted_res else int(formatted_res))
     except nothing:
         pass
     finally:
