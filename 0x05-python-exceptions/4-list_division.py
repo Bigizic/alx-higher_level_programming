4c61a82e46ba8fb0dabfdf677cf46cccafaaacba
Bigizic <olalekanisaac75@gmail.com>
2023-05-29 17:49:08 +0000

    Update list_division.py

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -5,22 +5,27 @@ def list_division(my_list_1, my_list_2, list_length):
     div = False
     max_range = False
     w_type = False
+    new_div = False
     try:
         for index, element in enumerate(my_list_1):
             if not isinstance(element, (int, float)):
                 my_list_1[index] = 0
                 div = True
+            if element == 0:
+                new_div = True
         for i, e in enumerate(my_list_2):
             if not isinstance(e, (int, float)):
                 my_list_2[i] = 0
                 div = True
+            if e == 0:
+                new_div = True
         for x in range(list_length):
             if x >= len(my_list_1) or x >= len(my_list_2):
                 result = 0
                 max_range = True
             elif my_list_1[x] == 0 or my_list_2[x] == 0:
                 result = 0
-                w_type = True
+                w_type = False if new_div == True else True
             else:
                 result = my_list_1[x] / my_list_2[x]
 
@@ -28,7 +33,7 @@ def list_division(my_list_1, my_list_2, list_length):
     except nothing:
         pass
     finally:
-        if div:
+        if div or new_div:
             print("division by 0")
         if w_type:
             print("wrong type")
