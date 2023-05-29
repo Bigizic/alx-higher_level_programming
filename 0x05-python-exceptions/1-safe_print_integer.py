0f93618197e59053c0a304935ffa0f7a2370334b
Bigizic <olalekanisaac75@gmail.com>
2023-05-29 12:54:26 +0000

    Update safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    if isinstance(value, int):
-        print("{:d}".format(value))
-        return (value)
+    try:
+        if isinstance(value, int):
+            print("{:d}".format(value))
+            return (True)
+        else:
+            return (False)
+    except exception_error:
+        pass
