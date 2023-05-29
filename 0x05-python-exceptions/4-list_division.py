40174b5c050e1335f09ff7500354f8e78c88da39
Bigizic <olalekanisaac75@gmail.com>
2023-05-29 17:35:48 +0000

    Update list_division.py

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = []
+    div = False
+    max_range = False
+    w_type = False
+    try:
+        for index, element in enumerate(my_list_1):
+            if not isinstance(element, (int, float)):
+                my_list_1[index] = 0
+                div = True
+        for i, e in enumerate(my_list_2):
+            if not isinstance(e, (int, float)):
+                my_list_2[i] = 0
+                div = True
+        for x in range(list_length):
+            if x >= len(my_list_1) or x >= len(my_list_2):
+                result = 0
+                max_range = True
+            elif my_list_1[x] == 0 or my_list_2[x] == 0:
+                result = 0
+                w_type = True
+            else:
+                result = my_list_1[x] / my_list_2[x]
+
+            new_list.append(result)
+    except nothing:
+        pass
+    finally:
+        if div:
+            print("division by 0")
+        if w_type:
+            print("wrong type")
+        if max_range:
+            print("out of range")
+        return new_list
