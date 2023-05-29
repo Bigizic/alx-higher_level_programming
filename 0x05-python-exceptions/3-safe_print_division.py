c74817196926ed382ded54b4b9b906205e659ace
Bigizic <olalekanisaac75@gmail.com>
2023-05-29 13:38:22 +0000

    Update safe_print_division.py

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    try:
+        if a == 0 or b == 0:
+            result = None
+        else:
+            result = a/b
+            result = float(result)
+    except zero_division:
+        pass
+    finally:
+        print("Inside result: {}".format(result))
+        return (result)
