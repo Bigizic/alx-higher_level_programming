30e255761ca7a378a2dbe80b6649f944c232aaa3
Bigizic <olalekanisaac75@gmail.com>
2023-05-31 22:21:04 +0000

    Update square.py

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,49 +1,40 @@
 #!/usr/bin/python3
 """Square module.
-This module contains a class that defines a square and its size and checking
-if the given values are right, and a setter and getter methods to set or get
-it. Some equivalence methods are present to help handles the use of
-comparators. There's also an area method that returns the area of the square.
+ a class Square that defines a square by: (based on 4-square.py)
 """
 
 
 class Square():
-    """Defines a square."""
+    """Square class"""
 
     def __init__(self, size=0):
-        """Sets the necessary attributes for the Square object.
+        """Constructor
         Args:
-            size (int): the size of one edge of the square.
+            size int type
         """
         self.size = size
 
     def __eq__(self, other):
-        """Sets the compare equality behavior of the Square object.
-        Args:
-            other (Square): the Square object to compare with.
+        """Set the Compare equality behavior of square object
         """
         if type(other) is Square:
             return self.area() == other.area()
 
     def __lt__(self, other):
-        """Sets the compare less than behavior of the Square object.
-        Args:
-            other (Square): the Square object to compare with.
+        """Set the compare less than behavior of the Square object
         """
         if type(other) is Square:
             return self.area() < other.area()
 
     def __le__(self, other):
-        """Sets the compare less equal than behavior of the Square object.
-        Args:
-            other (Square): the Square object to compare with.
+        """Sets the compare less equal than behavior of the Square object
         """
         if type(other) is Square:
             return self.area() <= other.area()
 
     @property
     def size(self):
-        """Get or set the size of the square."""
+        """Get or set the size of the square"""
         return self.__size
 
     @size.setter
@@ -57,6 +48,6 @@ class Square():
             raise TypeError("size must be an integer")
 
     def area(self):
-        """Returns the current square area."""
+        """Returns the current square area"""
 
         return self.__size ** 2
