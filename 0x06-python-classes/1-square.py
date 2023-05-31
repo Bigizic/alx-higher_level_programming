874bc45355929c0e5d9d5acec1f73bdbe0cedc40
Bigizic <olalekanisaac75@gmail.com>
2023-05-31 02:46:20 +0000

    Update square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Square module
+Defines a square based on 0-square.py
+"""
+
+
+class Square:
+    """Square class"""
+
+    def __init__(self, size):
+        """Sets sself"""
+        self.__size = size
