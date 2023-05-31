55e4cff23e590404c333a4d656689981de235fca
Bigizic <olalekanisaac75@gmail.com>
2023-05-31 02:30:34 +0000

    Update square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """ This is an empty class that defines a Square
 """
+
+
 class Square:
-    def __init__(self):
-        """ pass to indicate the function does nothing
-        """
-        pass
+    """ pass to indicate the Class does nothing
+    """
+    
+    pass
