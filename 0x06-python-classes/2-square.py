fd01a5ce242cba57b80bb5900f5a6d2f1708db1e
Bigizic <olalekanisaac75@gmail.com>
2023-05-31 02:53:34 +0000

    Update square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""Square module
+Class square that defines a square by: (based on 1-square.py)
+"""
+
+
+class Square:
+    """Class square"""
+
+    def __init__(self, size=0):
+        """sets the attributes for square object
+        Args:
+            size (int) type
+        Raises:
+            TypeError: if size is not given as integer
+            ValueError: if size is less than 0
+        """
+
+        if type(size) is int:
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError:("size must be an integer")
