157b570c7d45da4bb2f3cb5de87af743390c58f9
Bigizic <olalekanisaac75@gmail.com>
2023-05-31 16:59:55 +0000

    Update square.py

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+"""Square module
+ a class Square that defines a square by: (based on 5-square.py)
+ Public instance method: def my_print(self): that prints in stdout
+ the square with the character #:
+ if size is equal to 0, print an empty line
+ position should be use by using space - Don’t fill lines by spaces when
+ position[1] > 0
+ """
+
+
+class Square:
+    """Square class"""
+    
+    def __init__(self, size=0, position=(0, 0)):
+        """Constructor"""
+        self.size = size
+        self.position = position
+
+
+    @property
+    def size(self):
+        """Getter"""
+        return self.square_area
+
+
+    @size.setter
+    def size(self, value):
+        """Setter"""
+        if type(value) is int:
+            if value >= 0:
+                self.square_area = value
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+             raise TypeError("size must be an integer")
+
+    @property
+    def position(self):
+        """Position getter"""
+        return self.square_position
+
+
+    @position.setter
+    def position(self, value):
+        """Position setter"""
+        if not ((type(value) is tuple) or len(value) != 2 or not
+                all(isinstance(elements, int) and elements > 0 for i in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.square_position = value
+
+
+    def my_print(self):
+        """Prints in stdout the square with the character #"""
+        x = self.square_position[0]
+        y = self.square_position[1]
+
+        if self.square_area > 0:
+            for elements in range(y):
+                print()
+            for elements in range(self.square_area):
+                print(" " * x, end="")
+                print("#" * self.square_area)
+
+        else:
+            print()
+
+    def area(self):
+        """Returns square area"""
+        return self.square_area ** 2
