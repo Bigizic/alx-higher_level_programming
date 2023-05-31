34b4dfc08364b3e1d3da5b55ce22a65d5b3af91a
Bigizic <olalekanisaac75@gmail.com>
2023-05-31 12:32:43 +0000

    Update list_division.py

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -37,28 +37,29 @@ def list_division(my_list_1, my_list_2, list_length):
 
             if not isinstance(element, (int, float)):
                 my_list_1[index] = 0
+
                 """Wrong type error
                 sets wrong type to "True" to indicate an encountered
                 wrong type of element
                 """
-
                 wrong_type = True
         for i, e in enumerate(my_list_2):
             if not isinstance(e, (int, float)):
                 my_list_2[i] = 0
                 wrong_type = True
+
                 """List too short"
                 sets out of range error if a list length is zero
                 """
-
         if len(my_list_2) == 0:
             double_max_range = True
+
             """List_length
             iterate through the newly list1 and list2 and use a range of
             list_length to iterate through every element in the list
             """
-
         for x in range(list_length):
+
             """Out of range error
             if list_length is greater than the length of both list
             then out of range error is True and result is set to 0
@@ -67,27 +68,27 @@ def list_division(my_list_1, my_list_2, list_length):
             remaining elements to complete 7 elements in the list would
             ZEROS
             """
-
             if x >= len(my_list_1) or x >= len(my_list_2):
                 result = 0
                 max_range = True
+
                 """Extra check
                 if any element in list1 is 0 and if wrong_type
                 for either elements is true it append 0 to the
                 new list
                 """
-
             elif my_list_1[x] == 0 and wrong_type:
                 result = 0
+
                 """Division by 0 error
                 if any element in my list2 is zero then append 0
                 to new list and set the divsion by zero error to true
                 """
-
             elif my_list_2[x] == 0:
                 result = 0
                 div = True
-            result = my_list_1[x] / my_list_2[x]
+            else:
+                result = my_list_1[x] / my_list_2[x]
 
             new_list.append(result)
     except nothing:
