5d8deecb05daf7cf2010067364ade47c4e7970ef
Bigizic <olalekanisaac75@gmail.com>
2023-05-31 12:23:17 +0000

    Update list_division.py

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -2,6 +2,7 @@
 
 def list_division(my_list_1, my_list_2, list_length):
 
+
     """Documentation:
     This function divides elements in list1 by elements in list2
     both lists can contain any type
@@ -12,23 +13,25 @@ def list_division(my_list_1, my_list_2, list_length):
     if list1 or list2 is too short it prints "out of range"
     """
 
-    new_list = [] #Decleares a new list
+    new_list = [] # Decleares a new list
 
-    div = False #Handles error message for dision by 0
+    div = False # Handles error message for dision by 0
 
-    max_range = False #handles error message for out of range
+    max_range = False # handles error message for out of range
 
-    wrong_type = False #handles error message for wrong type
+    wrong_type = False # handles error message for wrong type
 
-    double_max_range = False #handles error message when a list too short
+    double_max_range = False # handles error message when a list too short
 
     try:
+
 
         """Iteration through list1
         this method gets the index and the elements in list1
         """
 
         for index, element in enumerate(my_list_1):
+
 
             """Elment type check
             a condition that checks if an element is not an int
@@ -39,6 +42,7 @@ def list_division(my_list_1, my_list_2, list_length):
 
             if not isinstance(element, (int, float)):
                 my_list_1[index] = 0
+
 
                 """Wrong type error
                 sets wrong type to "True" to indicate an encountered
@@ -51,6 +55,7 @@ def list_division(my_list_1, my_list_2, list_length):
                 my_list_2[i] = 0
                 wrong_type = True
 
+
                 """List too short"
                 sets out of range error if a list length is zero
                 """
@@ -58,12 +63,14 @@ def list_division(my_list_1, my_list_2, list_length):
         if len(my_list_2) == 0:
             double_max_range = True
 
+
             """List_length
             iterate through the newly list1 and list2 and use a range of
             list_length to iterate through every element in the list
             """
 
         for x in range(list_length):
+
 
             """Out of range error
             if list_length is greater than the length of both list
@@ -78,6 +85,7 @@ def list_division(my_list_1, my_list_2, list_length):
                 result = 0
                 max_range = True
 
+
                 """Extra check
                 if any element in list1 is 0 and if wrong_type
                 for either elements is true it append 0 to the
@@ -86,6 +94,7 @@ def list_division(my_list_1, my_list_2, list_length):
 
             elif my_list_1[x] == 0 and wrong_type:
                 result = 0
+
 
                 """Division by 0 error
                 if any element in my list2 is zero then append 0
