07112926b0ce8b808cfe1fd5cf8733b7afae165d
Bigizic <olalekanisaac75@gmail.com>
2023-05-31 12:56:30 +0000

    Update magic_calculation.py

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -7,7 +7,7 @@ def magic_calculation(a, b):
                 raise Exception('Too far')
             else:
                 result += (a ** b) / i
-        except nothing:
+        except Exception:
             result = a + b
             break
     return result
