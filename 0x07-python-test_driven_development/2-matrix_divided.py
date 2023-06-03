a36b8880a2d0b14dc23881ceace045697132d099
Bigizic <olalekanisaac75@gmail.com>
2023-06-03 11:25:17 +0000

    Update matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -14,43 +14,51 @@ def matrix_divided(matrix, div):
     div_number_error = "div must be a number"
     div_zero_error = "division by zero"
 
-    if type(matrix) is list:
+    if type(matrix) is not list:
         try:
-            row_length = len(matrix[0])
-            new_matrix = []
-            for rows in matrix:
-                if len(rows) == row_length:
-                    try:
-                        new_row = []
-                        for elements in rows:
-                            if type(elements) in [int, float]:
-                                try:
-                                    if type(div) in [int, float]:
-                                        try:
-                                            if div != 0:
-                                                try:
-                                                    new_element = round(elements / div, 2)
-                                                    new_row.append(new_element)
-                                                except Exception:
-                                                    raise ZeroDivisionError(div_zero_error)
-                                            else:
-                                                raise ZeroDivisionError(div_zero_error)
-                                        except Exception:
-                                            raise TypeError(div_number_error)
-                                    else:
-                                        raise TypeError(div_number_error)
-                                except Exception:
-                                    raise TypeError(matrix_error)
-                            else:
-                                raise TypeError(matrix_error)
-                        new_matrix.append(new_row)
-                    except Exception:
-                        raise TypeError(row_error)
-                else:
-                    raise TypeError(row_error)
-        except Exception:
+            raise TypeError(matrix_error)
+        except:
             raise TypeError(matrix_error)
     else:
-        raise TypeError(matrix_error)
+        matrix = matrix
 
+    if type(div) not in [int, float]:
+        try:
+            raise TypeError(div_number_error)
+        except Exception:
+            raise TypeError(div_number_error)
+    else:
+        div = div
+
+    if div == 0:
+        try:
+            raise ZeroDivisionError(div_zero_error)
+        except Exception:
+            raise ZeroDivisionError(div_zero_error)
+    else:
+        div = div
+
+
+    row_length = len(matrix[0])
+    new_matrix = []
+
+    for rows in matrix:
+        if len(rows) != row_length:
+            try:
+                raise TypeError(row_error)
+            except Exception:
+                raise TypeError(row_error)
+        else:
+            new_row = []
+
+        for elements in rows:
+            if type(elements) not in [int, float]:
+                try:
+                    raise TypeError(matrix_error)
+                except:
+                    raise TypeError(matrix_error)
+            else:
+                new_element = round(elements / div, 2)
+                new_row.append(new_element)
+        new_matrix.append(new_row)
     return new_matrix
