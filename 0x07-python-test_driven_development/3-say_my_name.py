f48d9ed66219ace86ba599514db570d5ebec8692
Bigizic <olalekanisaac75@gmail.com>
2023-06-05 04:34:10 +0000

    Update say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
 """Print name Module
- a function that prints My name is <first name> <last name>
- first_name and last_name must be strings otherwise, raise a TypeError
+
+Parameters:
+    first_name (str) first name
+    last_name (str) last name
 """
 
 
 def say_my_name(first_name, last_name=""):
+    """
+    A function that prints My name is <first name> <last name>
+    first_name and last_name must be strings otherwise, raise a TypeError
+
+    Raises:
+        TypeError
+    """
     if type(first_name) is not str:
         try:
             raise TypeError("first_name must be a string")
