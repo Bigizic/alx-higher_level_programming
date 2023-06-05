184b026300d0e9296269bad50afb95f39c6efc6e
Bigizic <olalekanisaac75@gmail.com>
2023-06-05 23:03:01 +0000

    Update nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -53,9 +53,8 @@ class NQueensSolver:
                         matches
                     )
                 group.append(self.pos[a].copy())
-                if not any(used_positions): self.build_solution(
-                        row + 1, group
-                    )
+                if not any(used_positions):
+                    self.build_solution(row + 1, group)
                 group.pop(len(group) - 1)
 
     def get_solutions(self):
