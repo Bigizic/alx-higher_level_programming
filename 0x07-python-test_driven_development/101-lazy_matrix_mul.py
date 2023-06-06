66e7a7afe4d1c90a5d69dfbf9aca6eb7e52d5be0
Bigizic <olalekanisaac75@gmail.com>
2023-06-06 14:59:16 +0000

    Update lazy_matrix_mul.py

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""A lazy Matrix Multiplication Module
+
+Parameters:
+    m_a (list) int or float types
+    m_b (list) int or float types
+"""
+import numpy as np
+
+def lazy_matrix_mul(m_a, m_b):
+    result = np.matmul(m_a, m_b)
+    return result
