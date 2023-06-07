2c033b25a4b5068265250f161be8556243c3b015
Bigizic <olalekanisaac75@gmail.com>
2023-06-07 23:54:03 +0000

    Update lazy_matrix_mul.py

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
-"""Numpy list multiplication module"""
+"""Numpy list multiplication module
+
+Parameters:
+    m_a (list of list of int/floats) first matrix
+    m_b (list of list of int/floats) second matrix
+
+Raises:
+    Void
+"""
 import numpy as np
 
 
 def lazy_matrix_mul(m_a, m_b):
+    """Returns the multiplication of two matrices
+    """
     result = np.matmul(m_a, m_b)
     return result
