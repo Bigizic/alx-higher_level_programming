0c248318ca6d3cbd9dc0c7d3de26c67b84aafd12
Bigizic <olalekanisaac75@gmail.com>
2023-06-08 00:27:04 +0000

    Update matrix_mul.py

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -9,7 +9,7 @@ Parameters:
 
 
 def matrix_mul(m_a, m_b):
-    """Displayed Errors"""
+    # Errors to display
     ma_error_1 = "m_a must be a list"
     mb_error_1 = "m_b must be a list"
     ma_list_error = "m_a must be a list of lists"
@@ -22,45 +22,53 @@ def matrix_mul(m_a, m_b):
     mb_rows_error = "each row of m_b must be of the same size"
     mul_error = "m_a and m_b can't be multiplied"
 
-    """List type error checks"""
+    # a check if m_a and m_b are either strings
     if type(m_a) is str:
         raise TypeError(ma_error_1)
 
     if type(m_b) is str:
         raise TypeError(mb_error_1)
 
+    # a check if either matrix are not a list or list of list
     if type(m_a) is not list or not all(isinstance(row, list) for row in m_a):
         raise TypeError(ma_list_error)
 
     if type(m_b) is not list or not all(isinstance(row, list) for row in m_b):
         raise TypeError(mb_list_error)
 
-    """Empty list error check"""
+    # a check if either matrix are a nested list i.e [[]]
+    if len(m_a) == 1 and len(m_a[0]) == 0:
+        raise ValueError(ma_empty_list)
+
+    if len(m_b) == 1 and len(m_b[0]) == 0:
+        raise ValueError(mb_empty_list)
+
+    # a check if either list is empty
     if len(m_a) == 0:
         raise ValueError(ma_empty_list)
 
     if len(m_b) == 0:
         raise ValueError(mb_empty_list)
 
-    """Check if all elements in both matrics are float or int types"""
+    # a check if all elements in either matrics are float or int types
     if not all(isinstance(num, (int, float)) for row in m_a for num in row):
         raise TypeError(ma_wrong_type)
 
     if not all(isinstance(num, (int, float)) for row in m_b for num in row):
         raise TypeError(mb_wrong_type)
 
-    """Check if both matrics have the same length of row"""
+    # a check if either matrics have the same length of row
     if len(set(len(row) for row in m_a)) != 1:
         raise TypeError(ma_rows_error)
 
     if len(set(len(row) for row in m_b)) != 1:
         raise TypeError(mb_rows_error)
 
-    """Get row and column of matrix a"""
+    # Get row and column of matrix a
     row_a = len(m_a)
     col_a = len(m_a[0])
 
-    """Get row and column of matrix b"""
+    # Get row and column of matrix b
     row_b = len(m_b)
     col_b = len(m_b[0])
 
@@ -72,9 +80,12 @@ def matrix_mul(m_a, m_b):
     for i in range(row_a):
         for j in range(col_b):
             for k in range(col_a):
+                # Final Result
                 result[i][j] += m_a[i][k] * m_b[k][j]
 
-    """Extra check to round the float types to 2 decimal places"""
+    """A check if the result contain float types that is/are not
+    2 decimal places after the multiplication
+    """
     for i in range(len(result)):
         for j in range(len(result[0])):
             if isinstance(result[i][j], float):
