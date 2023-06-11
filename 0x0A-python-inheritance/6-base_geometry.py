0651cbdba5306fedbe903eb479e81e0fe000b68d
Bigizic <olalekanisaac75@gmail.com>
2023-06-11 20:04:04 +0000

    Update base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Class Module
+
+Parameters:
+    Void
+
+Raise:
+    Exception("area() is not implemented")
+
+Return:
+    Void
+"""
+
+
+class BaseGeometry:
+    """Class BaseGeometry
+    """
+
+    def area(self):
+        raise Exception("area() is not implemented")
