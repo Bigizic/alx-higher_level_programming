d5cc3835463a8b913db51006c7aae82a394035d2
Bigizic <olalekanisaac75@gmail.com>
2023-06-11 20:24:19 +0000

    Update base_geometry.py

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Class Module
+
+Parameters:
+    name (str) is always a string
+    value (int)
+
+Raises:
+    Exception("area() is not implemented")
+    TypeError("<name> must be an integer")
+    ValueError("<name> must be greater than 0")
+
+Return:
+    Void
+"""
+
+
+class BaseGeometry:
+    """class BaseGeometry
+    """
+    m = "must be an integer"
+    n = "must be greater than 0"
+
+    def area(self):
+        """Raises exception message"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError("{} {}" .format(name, self.m))
+
+        if value <= 0:
+            raise ValueError("{} {}" .format(name, self.n))
