da914f4accec16b9ecdb1a8357412b5b19fafa94
Bigizic <olalekanisaac75@gmail.com>
2023-06-11 19:50:05 +0000

    Update inherits_from.py

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""A function that returns True if the object is an instance of a class
+that inherited (directly or indirectly) from the specified class,
+otherwise False
+
+Parameters:
+    obj (object) to be compared
+    a_class (class) specified class
+
+Return:
+    (True) if the object is an instance of a class that inherited
+    (False) otherwise
+"""
+
+
+def inherits_from(obj, a_class):
+    if type(obj) is not a_class or isinstance(obj, a_class):
+        return True
+    else:
+        return False
