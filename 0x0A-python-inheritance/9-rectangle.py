0b8adc82f94e770e732ed3d3d21356d570716e83
Bigizic <olalekanisaac75@gmail.com>
2023-06-11 21:37:13 +0000

    Update rectangle.py

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Class Module
+A class Rectangle that inherits from BaseGeometry
+
+Parameters:
+    width (int) width of rectangle
+    height (int) height of rectangle
+
+Return:
+    str() should return
+    the following rectangle description:[Rectangle] <width>/<height>
+"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """SubClass of BaseGeometry
+    """
+    
+    def __init__(self, width, height):
+    """constructor
+    """
+    self.integer_validator("width", width)
+    self.integer_validator("height", height)
+    self.my_width = width
+    self.my_height = height
+    
+    def __str__(self):
+    """Returns description
+    """
+    return ("[Rectangle] {}/{}" .format(self.my_width, self.my_height))
+
+    def area(self):
+    """Prints area of rectangle
+    """
+    result = self.my_width * self.my_height
+    print(result)
