d44cbd998accc11d31e1fd0ae3d5f05ae45a7c8a
Bigizic <olalekanisaac75@gmail.com>
2023-06-12 16:55:51 +0000

    Update my_int.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -14,11 +14,17 @@ class MyInt(int):
     """Subclass of int
     """
     def __init__(self, value):
+        """Constructor
+        """
         self.value = value
 
     def __eq__(self, other):
+        """Returns False
+        """
         return self.value != other
 
     def __ne__(self, other):
+        """Returns True
+        """
         return self.value == other
 
