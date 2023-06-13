a68c79357f958daf544faa0d2421a370c973f3f8
Bigizic <olalekanisaac75@gmail.com>
2023-06-13 15:22:01 +0000

    Update add_attribute.py

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -13,14 +13,20 @@ Return:
 """
 
 
-def add_attribute(obj, name, value):
+def add_attribute(self, name, value):
     """Adds a new attribute to an object if possible
-    if the object is a string or in any of those types it raises a
-    TypeError or if the object has the class attribute
+    if the self (object) is in any of those types it raises a
+    TypeError, becasue the object can only be a class. If the object
+    has an attribute called __slots__ i.e (slots sets the object to it's
+    given string) and if name not in the object's slot or name is not
+    equal to the object slot it raises a TypeError.
     otherwise setattr sets the object to the given name and value
     """
-    if (type(obj) in [str, int, float, None, bool] or
-            hasattr(obj, '__class__')):
+    if type(self) in [str, int, float, None, bool]:
         raise TypeError("can't add new attribute")
-
-    setattr(obj, name, value)
+    if hasattr(self, '__slots__'):
+        if name not in self.__slots__ or name != self.__slots__:
+            raise TypeError("can't add new attribute")
+        else:
+            setattr(self, name, value)
+    setattr(self, name, value)
