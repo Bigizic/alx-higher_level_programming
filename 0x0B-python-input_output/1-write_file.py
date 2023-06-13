5d7efe6480345b73a7d07f29d6ed5e454bd9dde4
Bigizic <olalekanisaac75@gmail.com>
2023-06-13 16:09:09 +0000

    Update write_file.py

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""A function that writes a string to a text file (UTF8)
+and returns the number of characters written
+
+Parameters:
+    filename (txt) name of the file to write into
+    text (str) text to be written
+
+Raises:
+    number of characters written
+
+Return:
+    void
+"""
+
+
+def write_file(filename="", text=""):
+    """Implementation
+    """
+    with open(filename, "w") as open_file:
+        write_to_file = open_file.write(text)
+        return len(text)
