fd345b80b5f5c8cc9ee8d0beb4ffc7b40692a75a
Bigizic <olalekanisaac75@gmail.com>
2023-06-13 22:58:10 +0000

    Update student.py

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""A class Student that defines a student by:
+    Public instance attributes:
+        first_name
+        last_name
+        age
+
+Parameters:
+    first_name (str)
+    last_name (str)
+    age (int)
+
+Raises:
+    Void
+
+Return:
+    Void
+"""
+
+
+class Student:
+    """Class Student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return vars(self)
