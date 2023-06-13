ec20ab71120bd9644acfb6515f6ea02988053028
Bigizic <olalekanisaac75@gmail.com>
2023-06-13 22:47:10 +0000

    Update class_to_json.py

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""A function that returns the dictionary description with simple data
+structure (list, dictionary, string, integer and boolean) for JSON
+serialization of an object
+
+Parameters:
+    obj (class) instance of a class
+
+Raises:
+    Void
+
+Return:
+     the dictionary description with simple data structure
+"""
+
+
+def class_to_json(obj):
+    a = vars(obj)
+    return a
