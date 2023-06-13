67987f7056649f27bfbd5b1ca9463bd26b56d6a6
Bigizic <olalekanisaac75@gmail.com>
2023-06-13 13:09:16 +0000

    Update add_attribute.py

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -16,10 +16,11 @@ Return:
 def add_attribute(obj, name, value):
     """Adds a new attribute to an object if possible
     if the object is a string or in any of those types it raises a
-    typeerror
+    TypeError or if the object has the class attribute
     otherwise setattr sets the object to the given name and value
     """
-    if type(obj) in [str, int, float, None, bool]:
+    if (type(obj) in [str, int, float, None, bool] or
+            hasattr(obj, '__class__')):
         raise TypeError("can't add new attribute")
 
     setattr(obj, name, value)
