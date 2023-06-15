ba7b3451c662b006a4fc0d3edc0c2c5a76e37433
Bigizic <olalekanisaac75@gmail.com>
2023-06-15 21:36:10 +0000

    Add models

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -35,12 +35,14 @@ class Square(Rectangle):
 
     @property
     def size(self):
-        """Getter for size, retrives height or width(anyone)
+        """Getter for size, retrives width
         """
         return (self.width)
 
     @size.setter
     def size(self, value):
+        """Setter for size, sets the width to a value
+        """
         if type(value) is int:
             if value > 0:
                 self.width = value
@@ -48,3 +50,22 @@ class Square(Rectangle):
                 raise ValueError("wdith must be > 0")
         else:
             raise TypeError("width must be an intger")
+
+    def update(self, *args, **kwargs):
+        """Assigns attributes
+        """
+        length = len(args)
+        if not args and len(args) < 1:
+            for key, value in kwargs.items():
+                if hasattr(self, key):
+                    setattr(self, key, value)
+
+        if (args):
+            if length >= 1:
+                self.id = args[0]
+            if length >= 2:
+                self.width = args[1]
+            if length >= 3:
+                self.x = args[2]
+            if length >= 4:
+                self.y = args[3]
