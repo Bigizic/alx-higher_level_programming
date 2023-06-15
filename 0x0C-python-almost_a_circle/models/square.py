de403f132e51d6ad46ef38abedc63843d0802152
Bigizic <olalekanisaac75@gmail.com>
2023-06-15 21:27:49 +0000

    Add models

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -32,3 +32,19 @@ class Square(Rectangle):
         """
         return ("[Square] ({}) {}/{} - {}" .format(self.id, self.x,
             self.y, self.width))
+
+    @property
+    def size(self):
+        """Getter for size, retrives height or width(anyone)
+        """
+        return (self.width)
+
+    @size.setter
+    def size(self, value):
+        if type(value) is int:
+            if value > 0:
+                self.width = value
+            else:
+                raise ValueError("wdith must be > 0")
+        else:
+            raise TypeError("width must be an intger")
