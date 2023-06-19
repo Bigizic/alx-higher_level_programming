1023fd1c9548242b31e41dbe82f43bd781c1f62f
Bigizic <olalekanisaac75@gmail.com>
2023-06-19 20:56:15 +0000

    Add models

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -40,7 +40,8 @@ class Rectangle(Base):
 
     def __str__(self):
         return ("[Rectangle] ({}) {}/{} - {}/{}"
-                .format(self.id, self.__x, self.__y, self.__width, self.__height))
+                .format(self.id, self.__x, self.__y,
+                    self.__width, self.__height))
 
     @property
     def width(self):
