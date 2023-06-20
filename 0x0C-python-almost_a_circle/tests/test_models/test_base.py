9f55c54281b0d1fa59d1c037cab0020e44dbcc2a
Bigizic <olalekanisaac75@gmail.com>
2023-06-20 00:17:29 +0000

    Add tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -15,11 +15,21 @@ from models.rectangle import Rectangle
 from models.square import Square
 
 class Test_base_fundamentals(unittest.TestCase):
+    """This Class contains test for the basic implementation of the Base
+    class
+
+    Tests:
+        For correct Base id
+    """
 
     def setUp(self):
+        """Setup
+        """
         Base._Base__nb_objects = 0
 
     def test_base_ids(self):
+        """Ids
+        """
         b1 = Base()
         self.assertEqual(b1.id, 1)
         b2 = Base()
@@ -33,11 +43,20 @@ class Test_base_fundamentals(unittest.TestCase):
 
 
 class Test_base_to_json_string(unittest.TestCase):
+    """Tests are:
+        Test for correct type
+        Test for length of text
+        Test for empty list exceptes []
+        """
 
     def setUp(self):
+        """Setup
+        """
         Base._Base__nb_objects = 0
 
     def test_to_json_string_type(self):
+        """Json String
+        """
         r1 = Rectangle(10, 7, 2, 8, 24)
         sq = Square(2, 1, 2, 42)
         self.assertEqual(str, type(
@@ -46,11 +65,15 @@ class Test_base_to_json_string(unittest.TestCase):
             Base.to_json_string([sq.to_dictionary()])))
 
     def test_to_json_string_length(self):
+        """Json string length
+        """
         r1 = Rectangle(10, 7, 2, 8, 24)
         dct = [r1.to_dictionary()]
         self.assertTrue(len(Base.to_json_string(dct)) == 54)
 
     def test_to_json_multiple_dictionary(self):
+        """Test multiple dictionareis
+        """
         r1 = Rectangle(2, 4, 1, 2, 42)
         sq = Square(2, 1, 2, 42)
 
@@ -58,8 +81,18 @@ class Test_base_to_json_string(unittest.TestCase):
         self.assertTrue(len(Base.to_json_string(dct)) == 92)
 
     def test_to_json_string_empty_list(self):
+        """Test empty list
+        """
         self.assertEqual(Base.to_json_string([]), '[]')
 
+
+class Test_base_save_to_file(unittest.TestCase):
+
+    def setUp(self):
+        Base._Base__nb_objects = 0
+
+    def test_save_to_file(self):
+        pass
 
 
 if __name__ == '__main__':
