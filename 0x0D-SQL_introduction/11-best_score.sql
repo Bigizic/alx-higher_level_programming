87479a6d94f16444118e69b77d32a265171c83e5
Bigizic <olalekanisaac75@gmail.com>
2023-07-18 22:09:52 +0000

    Update best_score.sql

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,6 +1,6 @@
 -- a script that lists all records with a score >= 10 in the table scnd_tb
 
-SELECT score, name
-FROM `hbtn_0c_0second_table`
+SELECT `score`, `name`
+FROM `hbtn_0c_0.second_table`
 WHERE `score` >= 10
 ORDER BY score DESC;
