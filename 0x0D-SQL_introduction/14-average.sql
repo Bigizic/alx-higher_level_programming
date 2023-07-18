edae1642644c202a566678cafdb606549fa43b52
Bigizic <olalekanisaac75@gmail.com>
2023-07-18 22:25:09 +0000

    Update average.sql

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,4 +1,4 @@
 -- a script that computes the score average of all records in the table
 
-FROM `second_table`
-SELECT AVG(`score`) AS `average`;
+SELECT AVG(`score`) AS `average`
+FROM `second_table`;
