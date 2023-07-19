d4d9fdf84b7f03964403ae8a2cee755e80fc0ca6
Bigizic <olalekanisaac75@gmail.com>
2023-07-19 21:33:06 +0000

    Update create_read_user.sql

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,7 @@
+-- a script that creates the database hbtn_0d_2 and the user user_0d_2
+-- user_0d_2 only have SELECT PRIVILAGE in the database created
+-- user_0d_2 has a password
+
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE DATABASE hbtn_0d_2;
+GRANT SELECT ON *hbtn_0d_2.table* TO 'user_0d_2'@'localhost';
