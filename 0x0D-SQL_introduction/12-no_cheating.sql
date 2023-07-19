0fd42591ec5913dda2c434d1e6e0edc9cc2e2595
Bigizic <olalekanisaac75@gmail.com>
2023-07-19 20:49:07 +0000

    Update no_cheating.sql

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,5 +1,8 @@
 -- a script that updates the score of Bob to 10 in the table second_table
 
-UPDATE `second_table`
-SET `score` = 10
-WHERE `name` = "Bob";
+UPDATE second_table
+SET score = 10
+WHERE name = "Bob";
+
+SELECT score, name FROM second_table
+ORDER BY score DESC;
