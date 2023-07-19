ac618a500b6f835d6ec7fe5a22ef7006caa113f1
Bigizic <olalekanisaac75@gmail.com>
2023-07-19 21:35:32 +0000

    Update create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,5 +3,5 @@
 -- user_0d_2 has a password
 
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-CREATE DATABASE hbtn_0d_2;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 GRANT SELECT ON *hbtn_0d_2.table* TO 'user_0d_2'@'localhost';
