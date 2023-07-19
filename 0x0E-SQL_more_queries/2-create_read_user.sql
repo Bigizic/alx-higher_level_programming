3d4f60387b749b06470911d1a2ee07464d0976a7
Bigizic <olalekanisaac75@gmail.com>
2023-07-19 21:37:29 +0000

    Update create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,9 @@
 -- a script that creates the database hbtn_0d_2 and the user user_0d_2
 -- user_0d_2 only have SELECT PRIVILAGE in the database created
 -- user_0d_2 has a password
+-- flush privilages at end
 
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-GRANT SELECT ON *hbtn_0d_2.table* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
