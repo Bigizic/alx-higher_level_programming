f3c9ae99f3a99402948e5a06ac6eb9e66eba6288
Bigizic <olalekanisaac75@gmail.com>
2023-07-19 21:10:27 +0000

    Update privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
--- a script that lists all privileges of the MySQL users
+-- a script that lists all privileges of the users
 
-SHOW GRANTS FOR user_0d_1@localhost;
-SHOW GRANTS FOR user_0d_2@localhost;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
