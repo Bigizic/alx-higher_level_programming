ab0821f6b02660647f2b4aec3b28e36f1bdb605a
Bigizic <olalekanisaac75@gmail.com>
2023-07-20 00:26:33 +0000

    Update not_my_genres.sql

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,12 @@
+--  a script that uses the hbtn_0d_tvshows database to list all genres
+-- not linked to the show Dexter
+
+SET @dexter_genre_id = (SELECT genre_id FROM tv_shows WHERE title = 'Dexter');
+SELECT name
+FROM tv_genres
+WHERE id NOT IN (
+	SELECT genre_id
+	FROM tv_show_genres
+	WHERE show_id = @dexter_genre_id
+)
+ORDER BY name
