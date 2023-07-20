7f5c3ca9e26e1b276c9afee33732e7b3f29ee5bf
Bigizic <olalekanisaac75@gmail.com>
2023-07-20 18:03:20 +0000

    Update rating_shows.sql

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -2,7 +2,7 @@
 -- Each record should display: tv_shows.title - rating sum
 -- Results must be sorted in descending order by the rating
 
-SELECT CONCAT(tv_shows.title, '   ', SUM(tv_show_ratings.rate)) AS `tv_show - rating sum`
+SELECT CONCAT(tv_shows.title, ' - ', SUM(tv_show_ratings.rate)) AS `tv_show - rating sum`
 FROM tv_shows
 LEFT JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
 GROUP BY tv_shows.title
