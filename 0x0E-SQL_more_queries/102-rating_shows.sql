175379c3a1658e5efbe1975e1eff951d3c1b8b21
Bigizic <olalekanisaac75@gmail.com>
2023-07-20 18:05:07 +0000

    Update rating_shows.sql

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,5 +1,5 @@
---  a script that lists all shows from hbtn_0d_tvshows_rate by their rating
--- Each record should display: tv_shows.title - rating sum
+-- a script that lists all shows from hbtn_0d_tvshows_rate by theirg
+-- rating, Each record should display: tv_shows.title - rating sum
 -- Results must be sorted in descending order by the rating
 
 SELECT CONCAT(tv_shows.title, ' - ', SUM(tv_show_ratings.rate)) AS `tv_show - rating sum`
