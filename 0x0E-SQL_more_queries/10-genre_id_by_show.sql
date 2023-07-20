1e21e93dbd520769698830e3a976021eff678579
Bigizic <olalekanisaac75@gmail.com>
2023-07-20 00:09:43 +0000

    Update genre_id_by_show.sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,8 +1,8 @@
 -- a script that lists all shows contained in hbtn_0d_tvshows that have
 -- at least one genre linked
 
-SELECT shows.title, genre.genre_id
-FROM tv_shows AS show
-INNER JOIN tv_show_genres AS genre
-ON shows.id = genre.show_id
-ORDER BY shows.title, genre.genre_id;
+SELECT sh.title, ge.genre_id
+FROM tv_shows AS sh
+INNER JOIN tv_show_genres AS ge
+ON sh.id = ge.show_id
+ORDER BY sh.title, ge.genre_id;
