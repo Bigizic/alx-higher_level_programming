b4f217a5d8e4b02606ff976bd74576aba63fe7ee
Bigizic <olalekanisaac75@gmail.com>
2023-08-14 16:51:16 +0000

    Update second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,5 +1,16 @@
 #!/usr/bin/node
 
+/* secondMaxNum - a function that creates a new array from the default array, it
+ * works by iterating through the array and appending any element that's a digit
+ * to the newArray as long as the element is not equal to the maximum element in
+ * the default array
+ *
+ * @max: (int) an integer that serves as the maximum integer from the default array
+ *
+ * @array: default array to perfrom certain operations on
+ *
+ * Return: void
+ */
 function secondMaxNum (max, array) {
   const newArray = [];
   let i = 0;
@@ -12,6 +23,15 @@ function secondMaxNum (max, array) {
   secondHelper(newArray);
 }
 
+/**
+ * secondHelper - this function prints the second max element in the array by going
+ * through the already altered array and checking for the maximum integer in the
+ * array
+ *
+ * @array: array to perfrom operation on
+ *
+ * Return: void
+ */
 function secondHelper (array) {
   let i = 0;
   let secMax = array[0];
@@ -24,6 +44,12 @@ function secondHelper (array) {
   console.log(secMax);
 }
 
+/**
+ * getMaximum - this function reads from stdin and gets the maximum number from
+ * input
+ *
+ * Return: void
+ */
 const getMaximum = () => {
   const args = process.argv.slice(2);
   let i = 0;
