89a532dc26bfbd46bbff8526a01a99d3ee6c7d71
Bigizic <olalekanisaac75@gmail.com>
2023-08-14 12:29:08 +0000

    Update to_integer.js

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const anInteger = () => {
+	const args = process.argv.slice(2);
+	const num = parseInt(args[0]);
+
+	if (isNaN(num))
+	{
+		console.log('Not a number');
+	} else {
+		console.log('My number: ' + num);
+	};
+};
+anInteger();
