7971ebfb6a11bbc447ccbf4f91d850ec54d12980
Bigizic <olalekanisaac75@gmail.com>
2023-08-14 12:33:42 +0000

    Update multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const loopToLang = () => {
+  const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+  let i = 0;
+  for (i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+};
+
+loopToLang();
