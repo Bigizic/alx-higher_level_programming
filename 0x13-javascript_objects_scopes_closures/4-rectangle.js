c44086678de5b49aaeae3058c922b8c39c4a17b1
Bigizic <olalekanisaac75@gmail.com>
2023-08-15 09:19:31 +0000

    Update rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,35 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0) {
+      return (0);
+    }
+    if (w && h) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let i = 0;
+    let word = '';
+    for (i = 0; i < this.height; i++) {
+      word = 'X'.repeat(this.width);
+      console.log(word);
+    }
+  }
+
+  rotate () {
+    const temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
+
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
+}
+
+module.exports = Rectangle;
