044c716a117e625461e033f0baecd1371370502d
Bigizic <olalekanisaac75@gmail.com>
2023-08-15 11:53:30 +0000

    Update sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,3 +1,19 @@
 #!/usr/bin/node
 
+const myDict = require('./101-data').dict;
 
+function sortedDict () {
+  const newDict = {};
+
+  for (const userId in myDict) {
+    const occurrences = myDict[userId];
+
+    if (!newDict[occurrences]) {
+      newDict[occurrences] = [];
+    }
+    newDict[occurrences].push(userId);
+  }
+  console.log(newDict);
+}
+
+sortedDict();
