a3996dd7193268e829444f0559424e913ae7ef6a
Bigizic <olalekanisaac75@gmail.com>
2023-08-18 12:13:50 +0000

    Update select_states.py

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""A script that list all states from the database hbtn_0e_0_usa
+"""
+
+import MySQLdb
+import sys
+
+
+def list_states():
+    """Establish a connection, execute an sql query, fetch and print results
+    """
+    stdin_args = sys.argv[1:]
+    user_name = stdin_args[0]
+    pass_word = stdin_args[1]
+    data_base = stdin_args[2]
+
+    connection = MySQLdb.connect(host = "localhost", port = 3306, user = user_name,
+            passwd = pass_word, db = data_base, charset = "utf8")
+    cursor = connection.cursor()
+    query = "SELECT * FROM states ORDER BY id ASC"
+    cursor.execute(query)
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
+
+    cursor.close()
+    connection.close()
+
+if __name__ == '__main__':
+    list_states()
