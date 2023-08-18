71f855b4ec7874e7eecf2ad5f48c5d82bd3bdf8f
Bigizic <olalekanisaac75@gmail.com>
2023-08-18 19:04:33 +0000

    Update cities_by_state.py

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+"""A script that lists all cities from a database assumong that there's
+a foreign key relationship between the cities and states table
+"""
+
+import MySQLdb
+import sys
+
+
+def cities_by_state():
+    """Implementation
+    """
+    user_name = args[1]
+    ps_w = args[2]
+    d_b = args[3]
+
+    con = MySQL.connect(host="localhost", port=3306, user=user_name,
+                        passwd=ps_w, db=d_b)
+    cursor = con.cursor()
+    queries = """
+    SELECT cities.id, cities.name AS city_name, states.name AS state_name
+    FROM cities
+    JOIN states ON cities.state_id = states.id
+    ORDER BY cities.id;
+    """
+
+    cursor.execute(queries)
+    results = cursor.fetchall()
+
+    for items in results:
+        print(items)
+
+    cursor.close()
+    con.close()
+
+
+if __name__ == '__main__':
+    cities_by_state()
