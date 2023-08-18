6e666159b519516871b955170c141793900655c6
Bigizic <olalekanisaac75@gmail.com>
2023-08-18 22:13:54 +0000

    Update model_state_filter_a.py

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -15,7 +15,7 @@ def model_state_filter_a():
     server running on localhost on port 3306, proceeds to list all State
     objects that contain the letter a from the database
     """
-    
+
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
                             sys.argv[1], sys.argv[2], sys.argv[3]))
 
@@ -23,8 +23,8 @@ def model_state_filter_a():
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    fetch_states = session.query(State).filter(State.name.like('%a%')).order_by(
-                   State.id)
+    fetch_states = session.query(State).filter(
+                   State.name.like('%a%')).order_by(State.id)
     for states in fetch_states:
         print(states.id, states.name, sep=": ")
 
