135ca129f01e80fc59275edb2a39877c025775be
Bigizic <olalekanisaac75@gmail.com>
2023-08-18 19:44:17 +0000

    4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -19,8 +19,9 @@ def cities_by_state():
                           passwd=ps_w, db=d_b, charset="utf8")
     cursor = con.cursor()
     queries = """
-    SELECT cities.id, cities.name
+    SELECT cities.id, cities.name AS city_name, states.name AS state_name
     FROM cities
+    JOIN states ON cities.state_id = states.id
     ORDER BY cities.id ASC;
     """
 
