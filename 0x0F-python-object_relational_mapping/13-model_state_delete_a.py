c1956e184e02e1ccd99fc3b1a9e77c5caef12410
Bigizic <olalekanisaac75@gmail.com>
2023-08-18 23:07:58 +0000

    Update model_state_delete_a.py

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""A script that deletes all State objects with a name containing the letter
+a from a database
+"""
+
+import sys
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+def model_state_delete_a():
+    """A function that takes 3 arguments from stdin, connects to a MySQL
+    server running on localhost at port 3306 and procceds to delete all State
+    objects with a name containing the letter a from a database
+    """
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                            sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    fetch_states = session.query(State).filter(State.name.like('%a%'))
+
+    if fetch_states:
+        session.delete(fetch_states)
+        session.commit()
+
+    session.close()
+
+
+if __name__ == '__main__':
+    model_state_delete_a()
