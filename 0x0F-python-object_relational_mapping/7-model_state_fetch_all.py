0349af0e1cc0fde4eaa51e15bb1bc2e693861243
Bigizic <olalekanisaac75@gmail.com>
2023-08-18 21:47:52 +0000

    Update model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -3,7 +3,7 @@
 """A script that lists all states objects from a database
 """
 
-from sqlalchemy import create_engine
+from sqlalchemy import (create_engine)
 from sqlalchcemy.orm import sessionmaker
 from model_state import Base, State
 import sys
@@ -13,15 +13,17 @@ def model_state_fetch_all():
     """A script that takes 3 arguments from stdin connect to a MySQL server
     running on localhost at port 3306
     """
-    engine = create_engine('mysql+mysqldb://{}:{}@ocalhost:3306/{}'.format(
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
                             sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    fetch_states = session.query(State).order_by(State.id).all()
+    fetch_states = session.query(State).order_by(State.id)
 
     for state in fetch_states:
-        print(state.id + ': ', state.name)
+        print(state.id, state.name, sep=": ")
 
     session.close()
 
