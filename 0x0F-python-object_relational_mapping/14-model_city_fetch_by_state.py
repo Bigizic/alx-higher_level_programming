7fec16a5f03540822a80446a1e11d63cd4791f6f
Bigizic <olalekanisaac75@gmail.com>
2023-08-19 10:18:23 +0000

    Update model_city_fetch_by_state.py

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,36 +1,33 @@
 #!/usr/bin/python3
-
-"""A script that prints all City objects from a database
+"""
+Script that prints all City objects from the database hbtn_0e_14_usa
 """
 
 import sys
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 from model_city import City
 
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
 
-def model_city_fetch_by_state():
-    """A function that takes 3 arguments from stdin, connects to a MySQL
-    server running on localhost at port 3306 and prints all City objects
-    from the database in ascending order by cities.id
-    """
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-                            sys.argv[1], sys.argv[2], sys.argv[3]))
-    # engine = create_engine(db_connect)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, database),
+                           pool_pre_ping=True)
+
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    fetch_states = session.query(State, City).filter(
-                   State.id == City.state_id).order_by(City.id)
+    cities = session.query(City).order_by(City.id).all()
 
-    for state, city in fetch_states:
-        print("{}: ({}) {}".format(state.name, city.id, city.name))
+    for city in cities:
+        state_name = session.query(State.name).filter(
+                     State.id == city.state_id).scalar()
+        print("{}: ({}) {}".format(state_name, city.id, city.name))
 
-    # session.commit()
     session.close()
-
-
-if __name__ == '__main__':
-    model_city_fetch_by_state()
