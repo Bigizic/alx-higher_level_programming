f11769d957ce5b48b415feb614038b46eb1a57a8
Bigizic <olalekanisaac75@gmail.com>
2023-08-19 10:46:00 +0000

    Add relationship_state.py

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -3,9 +3,12 @@
 Module containing improvement to the State class
 """
 
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, MetaData
 from sqlalchemy.orm import relationship
-from relationship_city import Base, City
+from sqlalchemy.ext.declarative import declarative_base
+
+my_meta_data = MetaData()
+Base = declarative_base(metadata=my_meta_data)
 
 
 class State(Base):
@@ -15,5 +18,4 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state",
-                          cascade="all, delete-orphan")
+    cities = relationship("City", backref="state")
