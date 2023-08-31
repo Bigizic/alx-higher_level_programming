5ead476fca47fe69823776fd983234d38e7ab352
Bigizic <olalekanisaac75@gmial.com>
2023-08-31 22:05:58 +0000

    Update peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,8 +4,10 @@
 def find_peak(list_of_integers):
     """Start
     """
-    if len(list_of_integers) is 0:
+    if len(list_of_integers) == 0:
         return None
+    if len(list_of_integers) <= 2:
+        return max(list_of_integers)
 
     lis = list_of_integers
     peak = None
