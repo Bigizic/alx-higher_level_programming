5b2975dacc07bcbf08853f108fc920dd6a36006a
Bigizic <olalekanisaac75@gmial.com>
2023-08-31 23:21:43 +0000

    Update post_json.sh

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a bash script sends a JSON POST request to a URL
-curl -sX POST -H "Content-Type: application/json" -d @"$2" "$1
+curl -sX POST -H "Content-Type: application/json" -d @"$2" "$1"
