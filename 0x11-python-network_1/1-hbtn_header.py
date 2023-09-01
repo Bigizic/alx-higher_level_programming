8b7256591eacf0d82c3b60df57b8fa473bae5076
Bigizic <olalekanisaac75@gmial.com>
2023-09-01 22:53:05 +0000

    Update hbtn_header.py

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""A python script that takes in a url, sends a request to the url
+and display the value of a particular variable found in the header
+of the response
+"""
+
+import urllib.request
+import sys
+
+address = sys.argv[1]
+
+with urllib.request.urlopen(address) as open_url:
+    get_variable = open_url.getheader('X-Request-Id')
+    print(get_variable)
