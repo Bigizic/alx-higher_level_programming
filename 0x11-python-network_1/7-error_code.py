b3987489de586a7421602618a17634365add79da
Bigizic <olalekanisaac75@gmial.com>
2023-09-01 23:48:46 +0000

    Update error_code.py

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""A python script that  takes in a URL, sends a request to the
+URL and displays the body of the response. handles http error
+"""
+
+import requests
+import sys
+
+
+if __name__ == '__main__':
+
+    address = sys.argv[1]
+    get_r = requests(address)
+
+    if get_r.status_code >= 400:
+        print("Error code: {}".format(get_r.status_code))
+    else:
+        print(get_r.text)
