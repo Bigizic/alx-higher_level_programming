3c179f04cc953ab3f5a533937ae76e09fedffaaf
Bigizic <olalekanisaac75@gmial.com>
2023-09-02 01:09:51 +0000

    Update github_commits.py

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""A script that connects to the github api and get commit info
+from a repo
+"""
+
+import sys
+import requests
+
+
+if __name__ == '__main__':
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+
+    api = f'https://api.github.com/repos/{owner}/{repo}/commits'
+    get_r = requests.get(api)
+    for commits in get_r.json():
+        sha = commits.get("sha")
+        author = commits.get("commit").get("author")
+        name = author.get("name")            
+        print("{}: {}".format(sha, name))
