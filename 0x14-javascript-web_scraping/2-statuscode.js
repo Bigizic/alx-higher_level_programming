74fb986d4094fe927e3730856a986263fa1c1025
Bigiizc <olalekanisaac75@gmail.com>
2023-09-26 14:25:01 +0100

    Update statuscode.js

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const getStatusCode = () => {
+  const url = process.argv[2];
+
+  const request = require('request');
+  request.get(url).on('response', function (response) {
+    console.log('code:', response.statusCode);
+  });
+};
+getStatusCode();
