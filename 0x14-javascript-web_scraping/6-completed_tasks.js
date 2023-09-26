a3a00a7421f0e94b6cb8aab781e9c051ad76ddfc
Bigiizc <olalekanisaac75@gmail.com>
2023-09-26 16:35:35 +0100

    Update completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -9,17 +9,15 @@ const numberOfCompletedTasks = () => {
   request.get(url, (error, response, body) => {
     if (error) throw error;
     const data = JSON.parse(body);
-    if (Array.isArray(data)) {
-      data.forEach((item, index) => {
-        if (!userIdList.includes(item.userId)) {
-          userIdList.push(item.userId);
-          completed[item.userId] = 0;
-        }
-        if (item.completed) {
-          completed[item.userId]++;
-        }
-      });
-    }
+    data.forEach((item, index) => {
+      if (!userIdList.includes(item.userId)) {
+        userIdList.push(item.userId);
+        completed[item.userId] = 0;
+      }
+      if (item.completed) {
+        completed[item.userId]++;
+      }
+    });
     /* console.log(userIdList); */
     console.log(completed);
   });
