330e2482b04d09831858981534523e704b0f4834
Bigiizc <olalekanisaac75@gmail.com>
2023-09-26 17:07:02 +0100

    Update starwars_characters.js

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+
+const printCharacters = () => {
+  const id = process.argv[2];
+  const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+  const request = require('request');
+  const actors = [];
+
+  request.get(url, (error, response, body) => {
+    if (error) throw error;
+    const data = JSON.parse(body);
+
+    async function fetchMyActors () {
+      for (const link of data.characters) {
+        await new Promise((resolve, reject) => {
+          request.get(link, (error, response, body) => {
+            if (error) reject(error);
+            const actorDetails = JSON.parse(body);
+            actors.push(actorDetails.name);
+            resolve();
+          });
+        });
+      }
+      for (const items in actors) {
+        console.log(actors[items]);
+      }
+    }
+    fetchMyActors();
+  });
+};
+printCharacters();
