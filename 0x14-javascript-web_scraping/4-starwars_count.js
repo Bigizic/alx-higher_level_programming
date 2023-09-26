ea69062c603cf66554bc7454752d4f835ffc9445
Bigiizc <olalekanisaac75@gmail.com>
2023-09-26 15:18:49 +0100

    Update starwars_count.js

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const characterCount = () => {
+  const url = process.argv[2];
+  const request = require('request');
+  let count = 0;
+  const charId = 'https://swapi-api.alx-tools.com/api/people/18/';
+
+  request.get(url, (error, response, body) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
+    const data = JSON.parse(body);
+    for (const item of data.results) {
+      if (Array.isArray(item.characters) && item.characters.includes(charId)) {
+        count += 1;
+      }
+    }
+    console.log(count);
+  });
+};
+characterCount();
