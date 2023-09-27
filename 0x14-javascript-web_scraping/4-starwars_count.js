c4abacab24a7a8b63dd083416d1473feb6cf20e6
Bigiizc <olalekanisaac75@gmail.com>
2023-09-27 13:42:08 +0100

    Update starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,23 +1,27 @@
 #!/usr/bin/node
 
 const characterCount = () => {
-  const url = process.argv[2];
-  const request = require('request');
-  let count = 0;
-  const charId = 'https://swapi-api.alx-tools.com/api/people/18/';
+	const url = process.argv[2];
+	const request = require('request');
+	let count = 0;
+	const charId = 'https://swapi-api.alx-tools.com/api/people/18/';
+	const temp = 'http://swapi.co/api/people/18/';
 
-  request.get(url, (error, response, body) => {
-    if (error) {
-      console.error(error);
-      return;
-    }
-    const data = JSON.parse(body);
-    for (const item of data.results) {
-      if (item.characters.includes(charId)) {
-        count += 1;
-      }
-    }
-    console.log(count);
-  });
+	request.get(url, (error, response, body) => {
+		if (error) {
+			console.error(error);
+			return;
+		}
+		const data = JSON.parse(body);
+		for (const item of data.results) {
+			if (Array.isArray(item.characters)) {
+				if (item.characters.includes(temp) ||
+					item.characters.includes(charId)) {
+					count += 1;
+				}
+			}
+		}
+		console.log(count);
+	});
 };
 characterCount();
